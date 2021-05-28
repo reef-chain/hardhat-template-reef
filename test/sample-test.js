8ac572bcac58bad0f17969756ee379051f6b5e4f
Žiga Franko <franko.ziga@gmail.com>
2021-05-28 18:02:16 +0200

    Evm provider update (#1)
    * hardhat reef updated
    
    * readme
    
    * initial test set to greeter contract

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,12 +1,12 @@
 const { assert } = require("chai");
 
-describe("Flipper", function() {
-  it("Should return the new flipped value once changed", async function() {
-    const Flipper = await hre.reef.getContractFactory("Flipper");
-    const flipper = await Flipper.deploy(true);
+describe("Greeter", function() {
+  it("Should return the new greetings value once changed", async function() {
+    const Greeter = await hre.reef.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello world");
 
-    assert.equal(await flipper.get(), true);
-    await flipper.flip();
-    assert.equal(await flipper.get(), false);
+    assert.equal(await greeter.greet(), "Hello world");
+    await greeter.setGreeting("This is a new greeting");
+    assert.equal(await greeter.greet(), "This is a new greeting");
   });
 });
